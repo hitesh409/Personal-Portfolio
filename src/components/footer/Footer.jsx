4a5ea3d10188c39a70231983e7fe711d93b27584
hitesh409 <98460671+hitesh409@users.noreply.github.com>
2023-04-21 11:30:12 +0530

    last commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,11 +11,10 @@ const footer = () => {
 
       <ul className="permalinks">
         
+        <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
-        <li><a href="#service">Service</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonial">Testimonials</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
 
